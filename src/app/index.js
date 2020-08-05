b9d3898b76283250f2c587962ffaee6c44972c11
Denis <d@f12.biz>
2020-08-05 06:49:14 -0700

    Adding a Dashboard Component 3:29

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,10 @@
-console.log("Hello World!!!");
-
 import { store } from './store';
-console.log(store.getState()); 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Dashboard } from './components/Dashboard'
+
+ReactDOM.render(
+    < Dashboard />,
+    document.getElementById("app")
+);
